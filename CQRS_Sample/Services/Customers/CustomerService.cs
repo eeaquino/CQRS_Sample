275876a875c3fdf0fb6dd05262254bd79e28d7a9
Edgar Aquino <eeaquino@hotmail.com>
2022-07-03 15:27:32 -0700

    Added service

---
+++ CQRS_Sample/Services/Customers/CustomerService.cs
@@ -0,0 +1,29 @@
+﻿using CQRS_Sample.Commands.Customers;
+using CQRS_Sample.Data.CommandHandlers;
+using CQRS_Sample.DTOs.Customers;
+using CQRS_Sample.Queries.Customers;
+
+namespace CQRS_Sample.Services.Customers
+{
+    public class CustomerService : IAPIService, ICustomerService
+    {
+        private readonly IMediator _mediator;
+
+        public CustomerService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task<CommandHandlerResult> ChangeCustomerCompany(int customerId, string company, CancellationToken ct)
+        {
+            var command = new ChangeCustomerCompany(customerId, company);
+            return _mediator.RunAsync(command, ct);
+        }
+
+        public async Task<CustomerDto?> GetCustomerById(int customerId, CancellationToken token)
+        {
+            var query = new GetCustomerById(customerId);
+            return await _mediator.RunAsync(query, token);
+        }
+    }
+}
---
+++ CQRS_Sample/Services/Customers/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using CQRS_Sample.Data.CommandHandlers;
+using CQRS_Sample.Data.Entities.Customers;
+using CQRS_Sample.DTOs.Customers;
+
+namespace CQRS_Sample.Services.Customers
+{
+    public interface ICustomerService
+    {
+        Task<CustomerDto?> GetCustomerById(int customerId, CancellationToken token);
+        Task<CommandHandlerResult> ChangeCustomerCompany(int customerId, string company, CancellationToken ct);
+    }
+}
