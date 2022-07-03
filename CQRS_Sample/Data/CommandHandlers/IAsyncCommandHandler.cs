408a2ac404acdd24ebff388bc4050a70e1ba024a
Edgar Aquino <eeaquino@hotmail.com>
2022-07-03 15:26:01 -0700

    Completed definition for command handler

--- CQRS_Sample/Data/CommandHandlers/IAsyncCommandHandler.cs
+++ CQRS_Sample/Data/CommandHandlers/IAsyncCommandHandler.cs
@@ -2,7 +2,7 @@
 
 namespace CQRS_Sample.Data.CommandHandlers
 {
-    public interface IAsyncCommandHandler<TCommand> where TCommand : ICommand
+    public interface IAsyncCommandHandler<in TCommand> where TCommand : ICommand
     {
         Task<CommandHandlerResult> Handle(TCommand command, CancellationToken ct);
     }
