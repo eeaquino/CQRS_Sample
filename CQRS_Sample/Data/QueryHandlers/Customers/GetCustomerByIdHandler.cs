6673dc0acc5cc7bae3fd72eaa2577d286f85e14d
Edgar Aquino <eeaquino@hotmail.com>
2022-07-03 15:26:57 -0700

    Created sample query

---
+++ CQRS_Sample/Data/QueryHandlers/Customers/GetCustomerByIdHandler.cs
@@ -0,0 +1,40 @@
+﻿using CQRS_Sample.Data.DbContexts;
+using CQRS_Sample.DTOs.Customers;
+using CQRS_Sample.Queries.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Sample.Data.QueryHandlers.Customers
+{
+    public sealed class GetCustomerByIdHandler : IAsyncQueryHandler<GetCustomerById, CustomerDto>
+    {
+        public MainDbContext _context;
+
+        public GetCustomerByIdHandler(MainDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<CustomerDto?> Handle(GetCustomerById query,CancellationToken ct)
+        {
+            var customer =await  _context.Customers
+                .Where(w => w.CustomerId == query.Id)
+                .Select(s=>new CustomerDto()
+                {
+                    Id = s.CustomerId,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Email = s.Email,
+                    Phone = s.Phone,
+                    Address = s.Address,
+                    City = s.City,
+                    State = s.State,
+                    PostalCode = s.PostalCode,
+                    Country = s.Country,
+                    Company = s.Company,
+                    Fax = s.Fax
+                })
+                .SingleOrDefaultAsync(ct);
+            return customer;
+        }
+
+    }
+}
---
+++ CQRS_Sample/Queries/Customers/GetCustomerById.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_Sample.DTOs.Customers;
+
+namespace CQRS_Sample.Queries.Customers
+{
+    public sealed class GetCustomerById : IQuery<CustomerDto>
+    {
+        public GetCustomerById(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; init; }
+    }
+}
